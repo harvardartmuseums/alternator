16f09c9911a9bf15e76f27ff66d42c860a46b2e2
Ian Callahan <callahan@hey.com>
2023-06-05 11:26:34 -0400

    Meta proof of concept

--- after.js
+++ after.js
@@ -1,4 +1,11 @@
 // this code will be executed after page load
 (function() {
-  console.log('after.js executed');
+  const config = { 
+    url: document.querySelector("meta[name='alternator-url']").getAttribute("content"),
+    contact: document.querySelector("meta[name='alternator-contact']").getAttribute("content"),
+    id: document.querySelector("meta[name='alternator-id']").getAttribute("content"),
+    contact: document.querySelector("meta[name='alternator-contact']").getAttribute("content"),
+    guide: document.querySelector("meta[name='alternator-guide']").getAttribute("content"),
+  }
+  console.log(config)
 })();
