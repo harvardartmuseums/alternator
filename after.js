7efdddd015e2e37d89ecda8de4e0b722a454e4bf
Ian Callahan <callahan@hey.com>
2023-06-05 12:19:44 -0400

    Sample form injection

--- after.js
+++ after.js
@@ -7,5 +7,19 @@
     contact: document.querySelector("meta[name='alternator-contact']").getAttribute("content"),
     guide: document.querySelector("meta[name='alternator-guide']").getAttribute("content"),
   }
+
+  const template = 
+  `<div style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: red; color: white; text-align: center;">
+    <form action="https://api.sheetmonkey.io/form/go6K41rAhxMTpjKuUAaASk" method="post">
+      <!-- Put HTML input fields in here and see how they fill up your sheet -->
+      <label>Example Field: <input type="text" name="Example Header" required /></label>
+      <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
+      <input type="submit" value="Submit" />
+    </form>
+    </div>
+  `;
+
+  const article = document.querySelector('article');
+  article.insertAdjacentHTML('afterend', template);
   console.log(config)
 })();
