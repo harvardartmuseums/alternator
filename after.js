d143d0bcae940782499dad7da1fc46c5bc120e48
Ian Callahan <callahan@hey.com>
2023-06-05 12:36:55 -0400

    Actual form fields with horrific styling

--- after.js
+++ after.js
@@ -6,14 +6,35 @@
     id: document.querySelector("meta[name='alternator-id']").getAttribute("content"),
     contact: document.querySelector("meta[name='alternator-contact']").getAttribute("content"),
     guide: document.querySelector("meta[name='alternator-guide']").getAttribute("content"),
+    target: document.querySelector("meta[name='alternator-target']").getAttribute("content"),
+    path: document.querySelector("meta[name='alternator-path']").getAttribute("content"),
+  }
+
+  var action = null;
+
+  switch(config.target) {
+    case "sm":
+      action = `https://api.sheetmonkey.io/form/${config.path}`
+      break;
+    case "gs":
+      action = `https://google.sheets`
+      break;
+    default:
+      action = null;
   }
 
   const template = 
   `<div style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: red; color: white; text-align: center;">
-    <form action="https://api.sheetmonkey.io/form/go6K41rAhxMTpjKuUAaASk" method="post">
+    <a href="${config.guide}">Institutional Style Guide</a>
+    <form action="${action}" method="post">
       <!-- Put HTML input fields in here and see how they fill up your sheet -->
-      <label>Example Field: <input type="text" name="Example Header" required /></label>
+      <label>Alt Text<textarea name="Alt" required /></textarea></label>
+      <label>Description<textarea name="Description" /></textarea></label>
+      <label>User Email<input type="text" name="User" /></label>
       <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
+      <input type="hidden" name="Id" value="${config.id}" />
+      <input type="hidden" name="Url" value="${config.url}" />
+      <input type="hidden" name="Contact" value="${config.contact}" />
       <input type="submit" value="Submit" />
     </form>
     </div>
