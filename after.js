9286c77f4406eb69e934f1f9578377e1f03195d7
catherinemaldonado <idiophonebox@gmail.com>
2023-06-05 15:05:04 -0400

    updating form styling

--- after.js
+++ after.js
@@ -24,20 +24,25 @@
   }
 
   const template = 
-  `<div style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: red; color: white; text-align: center;">
-    <a href="${config.guide}">Institutional Style Guide</a>
-    <form action="${action}" method="post">
-      <!-- Put HTML input fields in here and see how they fill up your sheet -->
-      <label>Alt Text<textarea name="Alt" required /></textarea></label>
-      <label>Description<textarea name="Description" /></textarea></label>
-      <label>User Email<input type="text" name="User" /></label>
-      <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
-      <input type="hidden" name="Id" value="${config.id}" />
-      <input type="hidden" name="Url" value="${config.url}" />
-      <input type="hidden" name="Contact" value="${config.contact}" />
-      <input type="submit" value="Submit" />
-    </form>
+  `<div style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: #d4d4d7; text-align: center; font-family: arial;" class="place-content-center">
+  <div style="width: 90%; margin: 0 auto; display:block; overflow:hidden;">
+  <a href="${config.guide}" target="_blank" style=" background-color: #444444; border: none; color: white; padding: 12px 32px; text-decoration: none; cursor: pointer; display:table; float:right; margin: 20px 0; font-size:16px;">Institutional Style Guide</a>
+  </div>
+  <form action="${action}" method="post" style="padding:20px 0; font-family: arial; width: 90%; margin: 0 auto;">
+    <!-- Put HTML input fields in here and see how they fill up your sheet -->
+    <textarea rows="3" placeholder="Alt text" name="alt" style="width:100%; font-family: arial; font-size:16px; margin-bottom: 10px; padding:10px; box-sizing: border-box;" required /></textarea>
+    <textarea rows="3" placeholder="Description text" name="description" style="width:100%; font-family: arial; font-size:16px; margin-bottom: 10px; padding:10px; box-sizing: border-box;" /></textarea>
+    <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
+    <input type="hidden" name="id" value="${config.id}" />
+    <input type="hidden" name="url" value="${config.url}" />
+    <input type="hidden" name="contact" value="${config.contact}" />
+    <div>
+    <input placeholder="Your Email" type="text" name="email" style="font-family: arial; font-size:16px; padding:10px;" />
+    <input style="font-size:16px; background-color: black; border: none; color: white; padding: 12px 32px; text-decoration: none; margin: 4px 2px;
+cursor: pointer;" type="submit" value="SUBMIT" />
     </div>
+  </form>
+  </div>
   `;
 
   const article = document.querySelector('article');
